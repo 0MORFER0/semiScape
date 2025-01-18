5a4615a75ca1fd0a94686627d042ac5162c443ae
0MORFER0 <34687990+0MORFER0@users.noreply.github.com>
2025-01-18 16:53:51 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const groupConfig = {
+  groupName: "Equipo Estrella",
+  members: ["Ana", "Carlos", "Sofía", "Miguel", "Laura"],
+  customMessage: "¡Bienvenidos, equipo estrella! Su misión es resolver este enigma."
+};
