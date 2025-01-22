bdc86f6a756dbce4a0b3a8202a651ccac01230ca
0MORFER0 <34687990+0MORFER0@users.noreply.github.com>
2025-01-22 17:22:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const groupConfig = {
   users: [
-    { name: "Richland", password: "password123" },
+    { name: "Richland", password: "12345" },
     { name: "Gerard", password: "12345" },
-    { name: "Maryann", password: "mypass" },
-    { name: "Amigos1", password: "escape1" },
-    { name: "Amigos2", password: "escape2" }
+    { name: "Maryann", password: "12345" },
+    { name: "Amigos1", password: "12345" },
+    { name: "Amigos2", password: "12345" }
   ]
 };
