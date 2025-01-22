1d15083c837cd7e35b0e152bbb677b7bdfa3c13c
0MORFER0 <34687990+0MORFER0@users.noreply.github.com>
2025-01-22 17:42:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,29 @@ const groupConfig = {
     "Cazador Sombrío", "Hechicera Lunar", "Espía Enmascarado", "Guardian Celestial", "Arquero Sagrado",
     "Bestia Salvaje", "Nómade del Viento", "Sombra Veloz", "Monje Elemental", "Forjador de Estrellas",
     "Sátiro Bailarín", "Nigromante Oscuro", "Diosa del Sol", "Heraldo del Vacío", "Alquimista Dorado"
-  ]
+  ],
+  trenes: [
+    { nombre: "Regidor", distancia: 121 },
+    { nombre: "Ribera", distancia: 83 },
+    { nombre: "Rabano", distancia: 157 },
+    { nombre: "Sierra", distancia: 203 },
+    { nombre: "Valle", distancia: 191 },
+    { nombre: "Cima", distancia: 137 },
+    { nombre: "Lago", distancia: 97 },
+    { nombre: "Costa", distancia: 113 },
+    { nombre: "Monte", distancia: 251 },
+    { nombre: "Sol", distancia: 161 },
+    { nombre: "Río", distancia: 103 },
+    { nombre: "Luna", distancia: 141 },
+    { nombre: "Norte", distancia: 221 },
+    { nombre: "Este", distancia: 53 },
+    { nombre: "Oeste", distancia: 61 },
+    { nombre: "Sur", distancia: 173 },
+    { nombre: "Puente", distancia: 139 },
+    { nombre: "Cielo", distancia: 163 },
+    { nombre: "Bosque", distancia: 131 },
+    { nombre: "Fuego", distancia: 199 }
+  ],
+  sumaDistanciasCorrectas: 361, // Suma correcta de las distancias
+  inicialesCorrectas: "RRR" // Iniciales correctas
 };
