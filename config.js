8c7b76455f9b5d40d30658ef4ea2e46d3266f349
0MORFER0 <34687990+0MORFER0@users.noreply.github.com>
2025-01-22 19:41:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,237 +1,32 @@
-{
-    "groupConfig": {
-        "users": [
-            {
-                "usuario": "Richland",
-                "contrase\u00f1a": "12345",
-                "carta": "Sombra Veloz",
-                "puntuacion": 40
-            },
-            {
-                "usuario": "Gerard",
-                "contrase\u00f1a": "12345",
-                "carta": "Forjador de Estrellas",
-                "puntuacion": 57
-            },
-            {
-                "usuario": "Maryann",
-                "contrase\u00f1a": "12345",
-                "carta": "Esp\u00eda Enmascarado",
-                "puntuacion": 85
-            },
-            {
-                "usuario": "Amigos1",
-                "contrase\u00f1a": "12345",
-                "carta": "Heraldo del Vac\u00edo",
-                "puntuacion": 76
-            },
-            {
-                "usuario": "Amigos2",
-                "contrase\u00f1a": "12345",
-                "carta": "Caballero Oscuro",
-                "puntuacion": 100
-            },
-            {
-                "usuario": "Carlos",
-                "contrase\u00f1a": "12345",
-                "carta": "Drag\u00f3n de Fuego",
-                "puntuacion": 38
-            },
-            {
-                "usuario": "Sofia",
-                "contrase\u00f1a": "12345",
-                "carta": "Alquimista Dorado",
-                "puntuacion": 92
-            },
-            {
-                "usuario": "Juan",
-                "contrase\u00f1a": "12345",
-                "carta": "Forjador de Estrellas",
-                "puntuacion": 68
-            },
-            {
-                "usuario": "Pedro",
-                "contrase\u00f1a": "12345",
-                "carta": "Monje Elemental",
-                "puntuacion": 74
-            },
-            {
-                "usuario": "Luc\u00eda",
-                "contrase\u00f1a": "12345",
-                "carta": "Mago Arcano",
-                "puntuacion": 41
-            },
-            {
-                "usuario": "Ana",
-                "contrase\u00f1a": "12345",
-                "carta": "Tit\u00e1n de la Tormenta",
-                "puntuacion": 23
-            },
-            {
-                "usuario": "David",
-                "contrase\u00f1a": "12345",
-                "carta": "Alquimista Dorado",
-                "puntuacion": 48
-            },
-            {
-                "usuario": "Esteban",
-                "contrase\u00f1a": "12345",
-                "carta": "F\u00e9nix Dorado",
-                "puntuacion": 49
-            },
-            {
-                "usuario": "Marta",
-                "contrase\u00f1a": "12345",
-                "carta": "Nigromante Oscuro",
-                "puntuacion": 53
-            },
-            {
-                "usuario": "Pablo",
-                "contrase\u00f1a": "12345",
-                "carta": "Hechicera Lunar",
-                "puntuacion": 93
-            },
-            {
-                "usuario": "Javier",
-                "contrase\u00f1a": "12345",
-                "carta": "Guerrero de Hielo",
-                "puntuacion": 38
-            },
-            {
-                "usuario": "Elena",
-                "contrase\u00f1a": "12345",
-                "carta": "Ladrona Fantasma",
-                "puntuacion": 91
-            },
-            {
-                "usuario": "Isabel",
-                "contrase\u00f1a": "12345",
-                "carta": "Hechicera Lunar",
-                "puntuacion": 82
-            },
-            {
-                "usuario": "Rosa",
-                "contrase\u00f1a": "12345",
-                "carta": "Ladrona Fantasma",
-                "puntuacion": 29
-            },
-            {
-                "usuario": "Luis",
-                "contrase\u00f1a": "12345",
-                "carta": "Diosa del Sol",
-                "puntuacion": 43
-            }
-        ],
-        "baraja": [
-            "Drag\u00f3n de Fuego",
-            "Caballero Oscuro",
-            "Sirena M\u00edstica",
-            "F\u00e9nix Dorado",
-            "Guerrero de Hielo",
-            "Mago Arcano",
-            "Tit\u00e1n de la Tormenta",
-            "Ladrona Fantasma",
-            "Rey de la Jungla",
-            "Vidente del Abismo",
-            "Cazador Sombr\u00edo",
-            "Hechicera Lunar",
-            "Esp\u00eda Enmascarado",
-            "Guardian Celestial",
-            "Arquero Sagrado",
-            "Bestia Salvaje",
-            "N\u00f3made del Viento",
-            "Sombra Veloz",
-            "Monje Elemental",
-            "Forjador de Estrellas",
-            "S\u00e1tiro Bailar\u00edn",
-            "Nigromante Oscuro",
-            "Diosa del Sol",
-            "Heraldo del Vac\u00edo",
-            "Alquimista Dorado"
-        ],
-        "trenes": [
-            {
-                "nombre": "Regidor",
-                "distancia": 121
-            },
-            {
-                "nombre": "Ribera",
-                "distancia": 83
-            },
-            {
-                "nombre": "Rabano",
-                "distancia": 157
-            },
-            {
-                "nombre": "Sierra",
-                "distancia": 203
-            },
-            {
-                "nombre": "Valle",
-                "distancia": 191
-            },
-            {
-                "nombre": "Cima",
-                "distancia": 137
-            },
-            {
-                "nombre": "Lago",
-                "distancia": 97
-            },
-            {
-                "nombre": "Costa",
-                "distancia": 113
-            },
-            {
-                "nombre": "Monte",
-                "distancia": 251
-            },
-            {
-                "nombre": "Sol",
-                "distancia": 161
-            },
-            {
-                "nombre": "R\u00edo",
-                "distancia": 103
-            },
-            {
-                "nombre": "Luna",
-                "distancia": 141
-            },
-            {
-                "nombre": "Norte",
-                "distancia": 221
-            },
-            {
-                "nombre": "Este",
-                "distancia": 53
-            },
-            {
-                "nombre": "Oeste",
-                "distancia": 61
-            },
-            {
-                "nombre": "Sur",
-                "distancia": 173
-            },
-            {
-                "nombre": "Puente",
-                "distancia": 139
-            },
-            {
-                "nombre": "Cielo",
-                "distancia": 163
-            },
-            {
-                "nombre": "Bosque",
-                "distancia": 131
-            },
-            {
-                "nombre": "Fuego",
-                "distancia": 199
-            }
-        ],
-        "sumaDistanciasCorrectas": 361,
-        "inicialesCorrectas": "RRR"
-    }
-}
+const groupConfig = {
+  users: [
+    { name: "Richland", password: "12345", carta: "Dragón de Fuego", puntuacion: 0 },
+    { name: "Gerard", password: "12345", carta: "Dragón de Fuego", puntuacion: 0 },
+    { name: "Maryann", password: "12345", carta: "Caballero Oscuro", puntuacion: 0 },
+    { name: "Amigos1", password: "12345", carta: "Caballero Oscuro", puntuacion: 0 },
+    { name: "Amigos2", password: "12345", carta: "Sirena Mística", puntuacion: 0 },
+    { name: "Lucas", password: "12345", carta: "Sirena Mística", puntuacion: 0 },
+    { name: "Carlos", password: "12345", carta: "Fénix Dorado", puntuacion: 0 },
+    { name: "Sofia", password: "12345", carta: "Fénix Dorado", puntuacion: 0 },
+    { name: "Ana", password: "12345", carta: "Guerrero de Hielo", puntuacion: 0 },
+    { name: "David", password: "12345", carta: "Guerrero de Hielo", puntuacion: 0 },
+    { name: "Pedro", password: "12345", carta: "Mago Arcano", puntuacion: 0 },
+    { name: "Eva", password: "12345", carta: "Mago Arcano", puntuacion: 0 },
+    { name: "Juan", password: "12345", carta: "Titán de la Tormenta", puntuacion: 0 },
+    { name: "Isabel", password: "12345", carta: "Titán de la Tormenta", puntuacion: 0 },
+    { name: "Maria", password: "12345", carta: "Ladrona Fantasma", puntuacion: 0 },
+    { name: "Ricardo", password: "12345", carta: "Ladrona Fantasma", puntuacion: 0 },
+    { name: "Laura", password: "12345", carta: "Rey de la Jungla", puntuacion: 0 },
+    { name: "Javier", password: "12345", carta: "Rey de la Jungla", puntuacion: 0 },
+    { name: "Sergio", password: "12345", carta: "Vidente del Abismo", puntuacion: 0 },
+    { name: "Alba", password: "12345", carta: "Vidente del Abismo", puntuacion: 0 }
+  ],
+  baraja: [
+    "Dragón de Fuego", "Caballero Oscuro", "Sirena Mística", "Fénix Dorado", "Guerrero de Hielo",
+    "Mago Arcano", "Titán de la Tormenta", "Ladrona Fantasma", "Rey de la Jungla", "Vidente del Abismo",
+    "Cazador Sombrío", "Hechicera Lunar", "Espía Enmascarado", "Guardian Celestial", "Arquero Sagrado",
+    "Bestia Salvaje", "Nómade del Viento", "Sombra Veloz", "Monje Elemental", "Forjador de Estrellas"
+  ],
+  sumaDistanciasCorrectas: 361, // Suma correcta de las distancias
+  inicialesCorrectas: "RRR" // Iniciales correctas
+};
