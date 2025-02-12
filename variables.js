c02b9067c994e80fa2b73cd6b5d6094d118e55c8
0MORFER0 <34687990+0MORFER0@users.noreply.github.com>
2025-02-12 19:43:04 +0100

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,28 @@
+export const usuarios = [
+  { nombre: "DANI MADRID", contraseña: "12345", cartaAsignada: "El Dragón Rojo", equipoEscondite: "1", abecedarioDesordenado: "QWERTYUIOPASDFGHJKLZXCVBNM", abecedarioDesordenadoPareja: "MNBVCXZLKJHGFDSAPOIUYTREWQ" },
+  { nombre: "ALBA ENFERMERA", contraseña: "12345", cartaAsignada: "El Dragón Rojo", equipoEscondite: "1", abecedarioDesordenado: "MNBVCXZLKJHGFDSAPOIUYTREWQ", abecedarioDesordenadoPareja: "QWERTYUIOPASDFGHJKLZXCVBNM" },
+
+  { nombre: "HELENA", contraseña: "12345", cartaAsignada: "El Lobo Gris", equipoEscondite: "2", abecedarioDesordenado: "ZXCVBNMASDFGHJKLQWERTYUIOP", abecedarioDesordenadoPareja: "POIUYTREWQLKJHGFDSAMNBVCXZ" },
+  { nombre: "VICTOR", contraseña: "12345", cartaAsignada: "El Lobo Gris", equipoEscondite: "2", abecedarioDesordenado: "POIUYTREWQLKJHGFDSAMNBVCXZ", abecedarioDesordenadoPareja: "ZXCVBNMASDFGHJKLQWERTYUIOP" },
+
+  { nombre: "RIAGUAS", contraseña: "12345", cartaAsignada: "La Serpiente Esmeralda", equipoEscondite: "1", abecedarioDesordenado: "ASDFGHJKLZXCVBNMQWERTYUIOP", abecedarioDesordenadoPareja: "PLMOKNIJBUVYCTRXEZWQSDGFAH" },
+  { nombre: "DANI GEMELO", contraseña: "12345", cartaAsignada: "La Serpiente Esmeralda", equipoEscondite: "1", abecedarioDesordenado: "PLMOKNIJBUVYCTRXEZWQSDGFAH", abecedarioDesordenadoPareja: "ASDFGHJKLZXCVBNMQWERTYUIOP" },
+
+  { nombre: "AMBRINOS", contraseña: "12345", cartaAsignada: "El Águila Negra", equipoEscondite: "2", abecedarioDesordenado: "MLPOKNIJBUVYCTRXEZWQSDGFAH", abecedarioDesordenadoPareja: "HGFDSAQWERTYUIOPLKJHZXCVBNM" },
+  { nombre: "DAVID", contraseña: "12345", cartaAsignada: "El Águila Negra", equipoEscondite: "2", abecedarioDesordenado: "HGFDSAQWERTYUIOPLKJHZXCVBNM", abecedarioDesordenadoPareja: "MLPOKNIJBUVYCTRXEZWQSDGFAH" },
+
+  { nombre: "GONZALO", contraseña: "12345", cartaAsignada: "El Cuervo Plateado", equipoEscondite: "1", abecedarioDesordenado: "LKJHGFDSAMNBVCXZPOIUYTREWQ", abecedarioDesordenadoPareja: "QAZWSXEDCRFVTGBYHNUJMIKOLP" },
+  { nombre: "RUBEN", contraseña: "12345", cartaAsignada: "El Cuervo Plateado", equipoEscondite: "1", abecedarioDesordenado: "QAZWSXEDCRFVTGBYHNUJMIKOLP", abecedarioDesordenadoPareja: "LKJHGFDSAMNBVCXZPOIUYTREWQ" },
+
+  { nombre: "ALFONSO", contraseña: "12345", cartaAsignada: "La Rosa Negra", equipoEscondite: "2", abecedarioDesordenado: "MKIOLPNUJHYGBVFTRDCXESZWAQ", abecedarioDesordenadoPareja: "WSXEDCRFVTGBYHNUJMIKOLPZAQ" },
+  { nombre: "SARA", contraseña: "12345", cartaAsignada: "La Rosa Negra", equipoEscondite: "2", abecedarioDesordenado: "WSXEDCRFVTGBYHNUJMIKOLPZAQ", abecedarioDesordenadoPareja: "MKIOLPNUJHYGBVFTRDCXESZWAQ" },
+
+  { nombre: "FIRE", contraseña: "12345", cartaAsignada: "El Camaleón Dorado", equipoEscondite: "1", abecedarioDesordenado: "PLMOKNIJBUVYCTRXEZWQSDGFAH", abecedarioDesordenadoPareja: "ASDFGHJKLZXCVBNMQWERTYUIOP" },
+  { nombre: "JOTA", contraseña: "12345", cartaAsignada: "El Camaleón Dorado", equipoEscondite: "1", abecedarioDesordenado: "ASDFGHJKLZXCVBNMQWERTYUIOP", abecedarioDesordenadoPareja: "PLMOKNIJBUVYCTRXEZWQSDGFAH" },
+
+  { nombre: "LORENA", contraseña: "12345", cartaAsignada: "El Espíritu del Bosque", equipoEscondite: "2", abecedarioDesordenado: "MNBVCXZLKJHGFDSAPOIUYTREWQ", abecedarioDesordenadoPareja: "QWERTYUIOPASDFGHJKLZXCVBNM" },
+  { nombre: "MANRY", contraseña: "12345", cartaAsignada: "El Espíritu del Bosque", equipoEscondite: "2", abecedarioDesordenado: "QWERTYUIOPASDFGHJKLZXCVBNM", abecedarioDesordenadoPareja: "MNBVCXZLKJHGFDSAPOIUYTREWQ" },
+
+  { nombre: "ALBA PERIODISTA", contraseña: "12345", cartaAsignada: "El Mago de Hierro", equipoEscondite: "2", abecedarioDesordenado: "ZXCVBNMASDFGHJKLQWERTYUIOP", abecedarioDesordenadoPareja: "POIUYTREWQLKJHGFDSAMNBVCXZ" },
+  { nombre: "RURU", contraseña: "12345", cartaAsignada: "El Mago de Hierro", equipoEscondite: "2", abecedarioDesordenado: "POIUYTREWQLKJHGFDSAMNBVCXZ", abecedarioDesordenadoPareja: "ZXCVBNMASDFGHJKLQWERTYUIOP" }
+];
